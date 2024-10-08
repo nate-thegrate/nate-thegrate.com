358757ce6d274e9286624e4d348fc601a6e16de1
Nate Wilson <nate.w5687@gmail.com>
2024-10-08 15:24:32 -0600

    mobile improvements

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"4013dc28a48b4fe1c1e9b25cc936f6d9477982e6","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"},{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"82c1dfcf588c2669ca391134910d634ca31fddf1","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"},{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2391482179"
+    serviceWorkerVersion: "391342015"
   }
 });
